8d5a46bdc275c5231ebb594a0fa2e588ea2fd9f6
Антон Травкин <antontravkin@MacBook-Pro-Anton.local>
2019-01-21 03:58:37 +0300

    new anim

--- js/main.js
+++ js/main.js
@@ -2,16 +2,28 @@ window.onload = function(){
 
     function scrollFunction() {
         var s = $(window).scrollTop();
+     
         console.log(s);
 
         $('.hello').css('transform', 'translate(' + -s + 'px, ' + s  + 'px)');
+        //$('.hello').css('transition-timing-function', 'step-start');
         $('.down').css('transform', 'translateY(' + s  + 'px)');
-        if (s > 100)
-            $('.down').fadeOut(400);
+        $('.wrap_item1').css('transform', 'translateY(' + s  + 'px)');
+        
+        if (s > 100)          
+            $('.down p, i').css('opacity', 0).css ('transition-duration', '.5s');
         else
-            $('.down').fadeIn(400);
-        if (s > 475)
-            $('.hello').css('transform', 'translate(' + -475  + 'px, ' + s  + 'px)');
+            $('.down p, i').css('opacity', 1);
+        if (s > 450){
+            $('.hello').css('transform', 'translate(' + -450  + 'px, ' + s  + 'px)');
+            $('.wrap_item1 h1, .developer').css('opacity', 1).css ('transition-duration', '.5s');
+        }
+        else{
+            $('.wrap_item1 h1, .developer').css('opacity', 0).css ('transition-duration', '.5s');
+        }
+            
+        
+
     }
     window.onscroll = scrollFunction; 
 
