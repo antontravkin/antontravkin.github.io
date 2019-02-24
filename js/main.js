db878f44f5085fc48af981d8526d5df2959174d9
Антон Травкин <antontravkin@MacBook-Pro-Anton.local>
2019-02-24 14:41:16 +0300

    1

--- js/main.js
+++ js/main.js
@@ -1,49 +1,42 @@
-window.onload = function(){
-    $('.hello').css('transform', 'translateX(' + 230 + 'px)');
-    function scrollFunction() {
-        var s = $(window).scrollTop();
-     
-        console.log(s);
-        $('.hello').css('transform', 'translate(' + (-s + 230) + 'px, ' + s  + 'px)');
-        $('.down').css('transform', 'translateY(' + s  + 'px)');
-        $('.wrap_item1').css('transform', 'translateY(' + s  + 'px)');
-        $('.mac').css('transform', 'translateY(' + s  + 'px)');
-        if (s > 100)          
-            $('.down p, .down i').css('opacity', 0).css ('transition-duration', '.5s');
-        else
-            $('.down p, i').css('opacity', 1);
-        if (s > 230){
-            $('.hello').css('transform', 'translate(' + 0  + 'px, ' + s  + 'px)');
-            $('.wrap_item1 h1, .developer').css('opacity', 1).css ('transition-duration', '.5s');
-        }
-        else{
-            $('.wrap_item1 h1, .developer').css('opacity', 0).css ('transition-duration', '.5s');
-        }
-        if (s > 300){
-            $('.macbook').css('opacity', 1).css ('transition-duration', '.5s');
-        }
-        else{
-            $('.macbook').css('opacity', 0).css ('transition-duration', '.5s');
-        }
-        if (s > 400){
-            $('.macbook').css('opacity', 0).css ('display', 'none');
-            $('.code').css('display', 'block');
-        }
-        else{
-            $('.code').css('display', 'none');
-            $('.macbook').css('display', 'block').css ('transition-duration', '.5s');
-        }
+$(document).ready(function() {
+    
 
-        if (s > 500){
-            $('.hello').css('transform', 'translateY(' + 500  + 'px)');
-            $('.down').css('transform', 'translateY(' + 500 + 'px)');
-            $('.wrap_item1').css('transform', 'translateY(' + 500  + 'px)');
-            $('.mac').css('transform', 'translateY(' + 500  + 'px)');
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+	$('.about__info').on('click', '.info', function() {
+        $(this).toggleClass('text_red').siblings('.info__text').slideToggle(400);
+        $(this).find('i').toggleClass('fa-minus-circle').toggleClass('fa-plus-circle');
+    });
+
+ 
+	
+    var a = true;   
+    function x(){
+        var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-36251023-1']);
+            _gaq.push(['_setDomainName', 'jqueryscript.net']);
+            _gaq.push(['_trackPageview']);
+        $('.circlechart').circlechart(); 
+        (function() {
+                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+        a = false;
+    } 
+    
+    var $win = $(window);
+    var $marker = $('.skills');
+    $win.scroll(function() {
+        if($win.scrollTop() + $win.height() >= $marker.offset().top) {
+            if(a)
+                x();
         }
-            
-        
-
-    }
-    window.onscroll = scrollFunction; 
-
-}
+    });
+    
+    
+    
+});
