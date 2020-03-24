dacd079e8f3bdf3138b90882feeed10eae4b10e2
antontravkin <travkin.rh@gmail.com>
2020-03-24 12:41:43 +0300

    очистка main.js Убрал паралакс

--- js/main.js
+++
@@ -1,7 +0,0 @@
-function parallax(event) {
-
-    //this.getElementById('tower').style.transform = translateX(event.clientX + 'px');
-    this.getElementById('tower').style.transform = 'translateX(' + (-event.clientX) / 100 + 'px)';
-}
-
-document.addEventListener('mousemove', parallax);
