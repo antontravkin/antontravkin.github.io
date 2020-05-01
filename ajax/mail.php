ffde9d69ffdc3b4ee7d42e3f3507f9b576923371
antontravkin <travkin.rh@gmail.com>
2020-05-01 11:46:40 +0300

    telegram form

--- ajax/mail.php
+++ ajax/mail.php
@@ -1,14 +1,38 @@
 <?php
-    $name = $_POST['name'];
-    $phone = $_POST['phone'];
-    $massege = $_POST['name'] . ' ' . $_POST['phone'];
-    $subject = "=?utf-8?B?".base64_encode("сообщение с сайта ShatoBarto.ru")."?=";
-    $headers = array(
-        'From' => 'webmaster@example.com',
-        'Reply-To' => 'webmaster@example.com',
-        'X-Mailer' => 'PHP/' . phpversion()
-    );
-
-    $success = mail('travkin.rh@gmail.com', $subject, $phone, $headers);
-    echo $success;
+ 
+/* https://api.telegram.org/botXXXXXXXXXXXXXXXXXXXXXXX/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+ 
+//Переменная $name,$phone, $mail получает данные при помощи метода POST из формы
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$massege = $_POST['massege'];
+ 
+//в переменную $token нужно вставить токен, который нам прислал @botFather
+$token = "1198395031:AAENC3uAlHv3DRMu65wDDLeqZnDBraEs8Xw";
+ 
+//нужна вставить chat_id (Как получить chad id, читайте ниже)
+$chat_id = "-405177077";
+ 
+//Далее создаем переменную, в которую помещаем PHP массив
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Сообщение' => $massege
+);
+ 
+//При помощи цикла перебираем массив и помещаем переменную $txt текст из массива $arr
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+ 
+//Осуществляется отправка данных в переменной $sendToTelegram
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+ 
+//Если сообщение отправлено, напишет "Thank you", если нет - "Error"
+if ($sendToTelegram) {
+  echo "Thank you";
+} else {
+  echo "Error";
+}
 ?>
