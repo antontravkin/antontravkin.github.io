76cd449f00366fd045a2ff34ece0a3d275d7171f
antontravkin <travkin.rh@gmail.com>
2021-06-14 13:11:44 +0300

    1

--- javascripts/app.js
+++ javascripts/app.js
@@ -6,9 +6,10 @@ function toJSONString(form) {
   var elements = form.querySelectorAll('input, select, textarea');
   
   for (var i = 0; i < elements.length; ++i) {
-    console.log(elements.length);
+    
     var element = elements[i];
     var name = element.name;
+    alert(name);
     var value = element.value;
     if (name) {
       obj[ name ] = value;
--- javascripts/dist/app.dev.js
+++ javascripts/dist/app.dev.js
@@ -8,9 +8,9 @@ function toJSONString(form) {
   var elements = form.querySelectorAll('input, select, textarea');
 
   for (var i = 0; i < elements.length; ++i) {
-    console.log(elements.length);
     var element = elements[i];
     var name = element.name;
+    alert(name);
     var value = element.value;
 
     if (name) {
