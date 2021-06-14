2d86251954993b18ca4e01534eb909e873fa6aa6
antontravkin <travkin.rh@gmail.com>
2021-06-14 10:58:04 +0300

    qwedf

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,25 +1,25 @@
 
-const formId = 'telegramForm'
-const form = document.getElementById(formId)
+const formId = 'telegramForm';
+const form = document.getElementById(formId);
 function toJSONString(form) {
-  var obj = {}
-  var elements = form.querySelectorAll('input, select, textarea')
+  var obj = {};
+  var elements = form.querySelectorAll('input, select, textarea');
   for (var i = 0; i < elements.length; ++i) {
     var element = elements[i]
-    var name = element.name
-    var value = element.value
+    var name = element.name;
+    var value = element.value;
     if (name) {
-      obj[ name ] = value
+      obj[ name ] = value;
     }
   }
-  return JSON.stringify(obj)
+  return JSON.stringify(obj);
 }
 if (form) {
   form.addEventListener('submit', event => {
-    event.preventDefault()
-    const json = toJSONString(form)
-    const formReq = new XMLHttpRequest()
-    formReq.open('POST', '/telegram', true)
+    event.preventDefault();
+    const json = toJSONString(form);
+    const formReq = new XMLHttpRequest();
+    formReq.open('POST', '/telegram', true);
     ///////////////////////////////////
     /////////////SweetAlert////////////
     formReq.onload = function(oEvent) {
@@ -29,22 +29,22 @@ if (form) {
           icon: 'success',
           timer: 2000
         })
-        document.querySelector('.sa-success').style.display = 'block'
-        document.querySelector('.sa-button-container').style.opacity = '0'
+        document.querySelector('.sa-success').style.display = 'block';
+        document.querySelector('.sa-button-container').style.opacity = '0';
       }
       if (formReq.status !== 200) {
         swal({
           title: 'Произошла ошибка!',
           icon: 'error',
           timer: 2000
-        })
-        document.querySelector('.sa-error').style.display = 'block'
-        document.querySelector('.sa-button-container').style.opacity = '0'
+        });
+        document.querySelector('.sa-error').style.display = 'block';
+        document.querySelector('.sa-button-container').style.opacity = '0';
       }
     }
     ////////////////////////////
     ////////////////////////////
-    formReq.setRequestHeader('Content-Type', 'application/json')
-    formReq.send(json)
-  })
+    formReq.setRequestHeader('Content-Type', 'application/json');
+    formReq.send(json);
+  });
 }
